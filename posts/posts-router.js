dc20428dfee5fb5a71c3a0d5893b1b13839c03dd
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-10-01 17:04:32 -0400

    Posts get and insert working.

---
+++ posts/posts-router.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const Posts = require('../data/db.js');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    Posts.find(req.body)
+    .then(post => {
+        res.status(201).json(post);
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'Error retrieving the posts',
+        });
+    });
+});
+
+router.post('/', (req, res) => {
+    Posts.insert(req.body)
+        .then(post => {
+            if (!req.body.title || !req.body.contents) {
+                res.status(400).json({
+                    message: 'No title or contents.'
+                })
+            } else {
+                res.status(201).json(post)
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: 'Error saving to database.'
+            })
+        })
+})
+
+router.get('/:id', (req, res) => {
+    Posts.findById(req.params.id) 
+    .then(post => {
+
+    })
+})
+
+module.exports = router; // don't forget to export or nothing works
